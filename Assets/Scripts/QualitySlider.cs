1aa18c357499e85af24b2e08ba47fe928799ad18
BurngDaylght <rada.raman543@gmail.com>
2025-11-24 12:17:30 +0300

    fix: update UI & minor bugs

--- Assets/Scripts/QualitySlider.cs
+++ Assets/Scripts/QualitySlider.cs
@@ -8,6 +8,13 @@ public class QualitySlider : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _valueText;
+    [SerializeField] private Image _circle;
+    [SerializeField] private float _punchDuration;
+    [SerializeField] private float _punchForce;
+    
+    private Vector3 _originalTextScale;
+    private Vector3 _originalCircleScale;
+    private int _lastIntValue;
     
     private FileProcessor _fileProcessor;
 
@@ -31,20 +38,41 @@ public class QualitySlider : MonoBehaviour
     
     private void Start()
     {
+        _lastIntValue = Mathf.RoundToInt(_slider.value);
+        
+        _originalTextScale = _valueText.transform.localScale;
+        _originalCircleScale = _circle.transform.localScale;
+
         UpdateText(_slider.value);
         _slider.onValueChanged.AddListener(OnValueChanged);
     }
     
     private void OnValueChanged(float value)
     {
-        UpdateText(_slider.value);
-        int crf = Mathf.RoundToInt(value); 
-        _fileProcessor.SetQuality(crf);
+        int intValue = Mathf.RoundToInt(value);
+    
+        if (intValue != _lastIntValue)
+        {
+            UpdateText(value);
+            AnimateUI();
+            _fileProcessor.SetQuality(intValue);
+
+            _lastIntValue = intValue;
+        }
     }
     
     private void UpdateText(float value)
     {
         _valueText.text = Mathf.RoundToInt(value).ToString();
+    }
+
+    private void AnimateUI()
+    {
+        _valueText.transform.DOKill(true);
+        _valueText.transform.DOPunchScale(_originalTextScale * _punchForce, _punchDuration, 10, 1f);
+
+        _circle.transform.DOKill(true);
+        _circle.transform.DOPunchScale(_originalCircleScale * _punchForce, _punchDuration, 10, 1f);
     }
 
     private void DisableSlider()
@@ -57,4 +85,3 @@ public class QualitySlider : MonoBehaviour
         _slider.interactable = true;
     }
 }
-
